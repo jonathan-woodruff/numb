6ac9746f801ecee18f9d7f583b1cc41002c7df38
Jonathan Woodruff <jonathanmwoodruff@gmail.com>
2024-03-15 11:22:30 -0500

    add sign-up page in the view

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import SignUp from './components/SignUp';
+/*
+import React, { useState, useEffect } from 'react';
+import { LocalizationProvider, DatePicker } from "@mui/x-date-pickers"
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
+import { Container, Grid, Button, Typography } from '@mui/material';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import { VictoryPie, VictoryTooltip } from 'victory';
+import Modal from './components/Modal';
+import ExpenseList from './components/ExpenseList';
+// import functions to interact with controller.
+import { fetchExpenses, expenseByCategory } from './utils';
+import './App.css';
+*/
+
+function App() {
+  return (
+    <>
+      <SignUp></SignUp>
+    </>
+  );
+};
+
+export default App;
